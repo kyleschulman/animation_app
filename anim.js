d8c8163cb4904dd4070fe279d7d2f62fd9e04ebd
Kyle Schulman <kyleschulman@Kyle-MBP.local>
2020-04-01 11:03:59 -0700

    b

---
+++ anim.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+			$("#Salad").click(function() {
+				$("#Salad")if (clicked == false) {
+      $(".box").animate({
+        width: "300px",
+        height: "300px",
+        marginLeft: "150px",
+        borderWidth: "10px",
+        opacity: 0.5
+      });
