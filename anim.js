9e975836b7cf0fd9854554a22a8a9d6d45b0b2cb
Kyle Schulman <kyleschulman@Kyle-MBP.local>
2020-04-02 20:09:24 -0700

    beautify

--- anim.js
+++ anim.js
@@ -1,24 +1,26 @@
-$("myText").keypress(function(event)) {
-        if (event.which = 49) { //press key 1
-      $("#salad").animate({transform: rotate(0.5turn);});
-		};
-			};
+$("myText").keypress(function (event)) {
+	if (event.which = 49) { //press key 1
+		$("#salad").animate({
+			transform: rotate(0.5 turn);
+		});
+	};
+};
 
 
-			$("#Burger").keypress(function(event) {
-        if (event.which = 50) //press key 2
-      $("#burger").animate({
-        transform: rotate(0.5turn);
-      });
-			});
+$("#Burger").keypress(function (event) {
+	if (event.which = 50) //press key 2
+		$("#burger").animate({
+			transform: rotate(0.5 turn);
+		});
+});
 
 
-			$("#Wings").keypress(function(event) {
-        if (event.which = 51) //press key 3
-      $("#wings").animate({
-        transform: rotate(0.5turn);
-      });
-			});
+$("#Wings").keypress(function (event) {
+	if (event.which = 51) //press key 3
+		$("#wings").animate({
+			transform: rotate(0.5 turn);
+		});
+});
 
 
 
@@ -28,8 +30,8 @@ $("myText").keypress(function(event)) {
 
 
 $(document).ready(function () {
-$("Salad").click(function () {
-	$(".salad")
+	$("Salad").click(function () {
+		$(".salad")
 			.animate({
 				width: "200px"
 			})
@@ -49,18 +51,16 @@ $("Salad").click(function () {
 });
 
 
-$(document).ready(function() {
-			$("#Salad").click(function() {
-				$("#Salad")if (clicked == false) {
-      $(".box").animate({
-        width: "300px",
-        height: "300px",
-        marginLeft: "150px",
-        borderWidth: "10px",
+$(document).ready(function () {
+			$("#Salad").click(function () {
+						$("#Salad") if (clicked == false) {
+								$(".box").animate({
+									width: "300px",
+									height: "300px",
+									marginLeft: "150px",
+									borderWidth: "10px",
 
-      });
-
-
+								});
 
 
 
@@ -70,49 +70,25 @@ $(document).ready(function() {
 
 
 
+								$(document).ready(function () {
+											$("#Salad").keypress(function (event) {
+														if (event.which = 49) { //press key 1
+															$("img").animate({
+																transform: rotate(0.5 turn);
+															});
+														});
 
 
+													$(document).ready(function () {
+															$("#Burger").keypress(function (event) {
+																		if (event.which = 50) //press key 2
+																			$("img").animate({
+																				transform: rotate(0.5 turn);
+																			});
 
-
-
-
-
-
-
-
-
-
-
- $(document).ready(function() {
-			$("#Salad").keypress(function(event) {
-        if (event.which = 49) { //press key 1
-      $("img").animate({
-        transform: rotate(0.5turn);
-      });
-		});
-
-
-   $(document).ready(function() {
-			$("#Burger").keypress(function(event) {
-        if (event.which = 50) //press key 2
-      $("img").animate({
-        transform: rotate(0.5turn);
-      });
-
-    $(document).ready(function() {
-			$("#Wings").keypress(function(event) {
-        if (event.which = 51) //press key 3
-      $("img").animate({
-        transform: rotate(0.5turn);
-      });
-
-
-
-
-
-
-
-
-
-
-
+																		$(document).ready(function () {
+																					$("#Wings").keypress(function (event) {
+																								if (event.which = 51) //press key 3
+																									$("img").animate({
+																										transform: rotate(0.5 turn);
+																									});
